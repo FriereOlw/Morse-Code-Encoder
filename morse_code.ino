0782f92f9bae84d9a05e4a6abfedf77b52bc5d2a
farsec <fuadi1609@gmail.com>
2025-09-14 22:42:35 +0800

    refactor

--- morse_code.ino
+++ morse_code.ino
@@ -1,76 +1,58 @@
 
-const int delay_time{100}; // DELAY FOR . 
-const int PIN_ONE{3}; // PIN FOR OUTPUT 1
-
-#ifdef TWO_OUTPUT
-const int PIN_TWO{2}; // PIN FOR OUTPUT 2
-#endif
+const int DELAY_TIME{100}; // Delay for dot
+const int PIN_ONE{3}; // Pin for buzzer
 
 
 void setup() {
   Serial.begin(9600);  
   pinMode(PIN_ONE, OUTPUT);
-
-#ifdef TWO_OUTPUT
-  pinMode(PIN_TWO, OUTPUT);
-#endif
 }
 
 
-void sendSignal(char symbols){ //SEND EACH LETTER   
-#ifndef TWO_OUTPUT
+void sendSignal(char signal){ 
   digitalWrite(PIN_ONE, HIGH);
-  if(symbols == '-'){
-    delay(delay_time*3);
-  } else {
-    delay(delay_time);
+  if(signal == '-'){
+    delay(DELAY_TIME*3);
+  } 
+  else {
+    delay(DELAY_TIME);
   }
   digitalWrite(PIN_ONE, LOW);
-
-#else
-  if(symbols == '-'){
-    digitalWrite(PIN_ONE, HIGH);
-    delay(delay_time*3);
-    digitalWrite(PIN_ONE, LOW);
-  } else {
-    digitalWrite(PIN_TWO, HIGH);
-    delay(delay_time);
-    digitalWrite(PIN_TWO, LOW);
-  }
-#endif
 }
 
 
-void sendMorse(char letter){ //CONVERT LETTER TO CORRESPONGING MORSE CODE 
-  const String morse_codes[27] = {"..-","-...","-.-.","-..",".","..-.","--.",
+void sendMorse(char letter){ // Encode letter to code
+  const char* morse_codes[26] = {".-","-...","-.-.","-..",".","..-.","--.",
                       "....","..",".---","-.-",".-..","--","-.",
                       "---",".--.","--.-",".-.","...","-","..-",
                       "...-",".--","-..-","-.--","--.."};
 
-  int char_a = letter > 'A' && letter <= 'Z' ? 65 : 97;
-  String morse = {morse_codes[letter-char_a]};
+  letter = tolower(letter)-'a';
+  if (letter < 0 || letter > 25)
+    return;
 
-  for(int i = 0; morse[i] != '\0'; i++){
+  String morse = {morse_codes[letter]};
+
+  for(int i = 0; i < morse.length(); i++){
     Serial.print(morse[i]);
-    sendSignal(morse[i]); //ARGUMENT IS 0/1 
-    if (i+1 != '\0') 
-      delay(delay_time);
+    sendSignal(morse[i]); // 
+    if (i < morse.length()-1)
+      delay(DELAY_TIME);
   }
 }
 
 
-void readWord(String& word){ //SPLIT WORD TO EACH LETTER
-  int len{word.length()};
-
-  for (int i = 0; i < len; i++){
-    if(word[i] == ' '){ //Space between word
-      Serial.print('\n');
-      delay(delay_time*7);
+void readText(String& text){
+  for (int i = 0; i < text.length(); i++){
+    if(text[i] == ' '){ // Space between word
+      Serial.print('#');
+      delay(DELAY_TIME*7);
       continue;
     }
-    sendMorse(word[i]); //ARGUMENT IS EACH LETTER
-    Serial.print('#');
-    delay(delay_time*3);
+    sendMorse(text[i]); // Argument is each letter
+    if (i < text.length() && text[i+1] != ' ') // "/" between letter
+      Serial.print('/');
+    delay(DELAY_TIME*3);
   }
 
   Serial.print('\n');
@@ -78,6 +60,8 @@ void readWord(String& word){ //SPLIT WORD TO EACH LETTER
 
 
 void loop() {
-  String word{Serial.readString()};
-  readWord(word);
+  if (Serial.available()) {
+    String text{Serial.readString()};
+    readText(text);
+  }
 }
